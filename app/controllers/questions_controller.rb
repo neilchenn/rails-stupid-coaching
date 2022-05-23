bdbaab0c5878681399caa4ad97825e0593fe5a78
neil chen <zooming.in@hotmail.com>
2022-05-23 14:20:40 +1000

    updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    # if params[:input] == "I am going to work"
-    # end
+    if params[:input] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:input].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
